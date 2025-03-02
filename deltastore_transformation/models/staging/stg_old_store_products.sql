dad9fed5831e73d008df5eafb2a00c4472ed9b0f
hammariheb <iheb.hammar.officiel@gmail.com>
2025-03-02 16:30:57 +0100

    creating countries files in seeds and standadizing some folder names

--- deltastore_transformation/models/staging/stg_old_store_products.sql
+++ deltastore_transformation/models/staging/stg_old_store_products.sql
@@ -5,8 +5,8 @@ with source as (
 
 products as (
     select 
-        product_id,
-        product_name,
+         product_id,
+         product_name,
             case 
             when product_name is not null 
             then 'This is a ' + product_name + ' in the category ' + category
@@ -15,7 +15,7 @@ products as (
         category,
         cast(round(price,2) as float) as price,
         supplier_id,
-        'old_store' as source_store,
+        'old store' as source_store,
         updated_at
     from source
 )
